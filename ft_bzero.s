fd26fc55ee1b818b08fa1ee0df8b277ab3244fc2
Ryan David Sheasby <ryan27968@gmail.com>
2018-08-21 21:12:06 +0200

    Slim down ft_bzero

--- ft_bzero.s
+++ ft_bzero.s
@@ -2,14 +2,14 @@ section .text
     global ft_bzero 
 
 ft_bzero:
+; Validate input
     cmp rsi, 0 ; ensure length is greater than 0
     jle end
     cmp rdi, 0 ; ensure pointer is not null
     je end
+
+; Zero the array
     mov rax, rdi ; array pointer
     mov rcx, rsi ; array length
     mov al, 0    ; byte to store
-l1:
-    rep stosb
-end:
     ret
