2c4ce049b70edf7dbb677ebceb8ae08f9cb38f9d
Ryan David Sheasby <ryan27968@gmail.com>
2018-08-22 22:04:26 +0200

    Add ft_memcpy input validation and comments

--- ft_memcpy.s
+++ ft_memcpy.s
@@ -2,7 +2,18 @@ section .text
     global ft_memcpy
 
 ft_memcpy:
-    mov rcx, rdx
-    cld
-    rep movsb
-    ret
+; Validate input
+    cmp rdi, 0 ; ensure dest is not null
+    je end     ;
+    cmp rsi, 0 ; ensure src is not null
+    je end     ;
+    cmp rdx, 0 ; ensure len is not null
+    je end     ;
+
+; Copy memory
+    mov rcx, rdx ; how many bytes to copy
+    cld          ; move from left to right
+    rep movsb    ; copy bytes
+
+end:
+    ret ; end
