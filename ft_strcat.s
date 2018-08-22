28d7316d0c151c4c89b76165dd1e237cb0591247
Ryan David Sheasby <ryan27968@gmail.com>
2018-08-22 08:20:21 +0200

    Improve ft_strcat comments

--- ft_strcat.s
+++ ft_strcat.s
@@ -9,20 +9,20 @@ ft_strcat:
     je end
 
 ; Move dest pointer to end of dest
-    push rdi   ; preserve dest pointer
-    push rsi   ; preserve src pointer
-    cld
-    mov rcx, 0 ; set rcx to max value
+    push rdi    ; preserve dest pointer
+    push rsi    ; preserve src pointer
+    cld         ; move from left to right
+    mov rcx, 0  ; set rcx to max value
     not rcx
-    mov al, 0  ; byte to search for
-    repne scasb
-    dec rdi    ; move dest pointer to point to null byte
+    mov al, 0   ; byte to search for
+    repne scasb ; scan for end
+    dec rdi     ; move dest pointer to point to null byte
 
 ; Get length of src
     xchg rdi, rsi ; swap src and dest pointers
     mov rcx, 0    ; set rcx to max value
     not rcx
-    repne scasb
+    repne scasb   ; scan for end
     not rcx       ; length of src including null byte
 
 ; Copy src to end of dest
