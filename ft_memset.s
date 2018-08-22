4b33da314577221f92b4cfea1ed2e8df02b640b5
Ryan David Sheasby <ryan27968@gmail.com>
2018-08-22 21:47:35 +0200

    Add input validation and comments to ft_memset

--- ft_memset.s
+++ ft_memset.s
@@ -2,8 +2,16 @@ section .text
     global ft_memset
 
 ft_memset:
-    mov ax, si
-    mov rcx, rdx
-    cld
-    rep stosb
-    ret
+; Validate input
+    push rdi   ; preserve original memory pointer
+    cmp rdi, 0 ; ensure string pointer isn't null
+    je end     ;
+
+    mov ax, si   ; character to write
+    mov rcx, rdx ; length of memory to write
+    cld          ; move from left to right
+    rep stosb    ; write bytes
+
+end:
+    pop rax ; restore original memory pointer
+    ret     ; end
