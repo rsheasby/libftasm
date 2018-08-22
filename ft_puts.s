3c049a65dd55dbe898775ccf9a8d41f711fc79c5
Ryan David Sheasby <ryan27968@gmail.com>
2018-08-22 21:13:37 +0200

    Refactor ft_puts

--- ft_puts.s
+++ ft_puts.s
@@ -1,24 +1,37 @@
 section .text
     global ft_puts
 
-ft_puts: ;set values for write syscall
-    mov rsi, rdi ;buffer pointer
-    mov rdx, 0 ;string length
-    mov rdi, 1 ;file descriptor
-    mov rax, 1 ;use sys_write
-strlen: ;count length of string.
-    cmp byte [rsi + rdx], 0 ;check if byte null
-    je strlen_end ;if it is, end
-    inc rdx ;otherwise, increment counter
-    jmp strlen ;and loop
-strlen_end:
-    syscall ;ask kernel to write out string
-newline:
-    mov rax, `\n`
-    push rax ;put a newline on the stack and print it out.
-    mov rax, 1
-    mov rdx, 1
-    mov rsi, rsp
-    syscall
-    add rsp, 8
-    ret ;end
+ft_puts:
+; Validate input
+    mov rax, -1 ; if validation fails, return -1 (EOF)
+    cmp rdi, 0 ; ensure string pointer isn't null
+    je end
+
+; Get length of string
+    push rdi    ; preserve original pointer
+    cld         ; move from left to right
+    mov rcx, 0  ; set rcx to max value
+    not rcx
+    mov al, 0   ; byte to search for
+    repne scasb ; scan for end
+    not rcx
+    dec rcx     ; length of string excluding null byte
+
+; Print out string
+    mov rax, 1   ; use "write" syscall
+    mov rdi, 1   ; write to stdout
+    pop rsi      ; restore original string pointer
+    mov rdx, rcx ; length of string
+    syscall      ; print out string
+
+; Print newline
+    mov rax, 1    ; use "write" syscall
+    mov rsi, `\n` ; push newline to stack
+    push rsi
+    mov rdx, 1    ; write 1 byte
+    mov rsi, rsp  ; pointer to byte on stack
+    syscall       ; write out char
+    add rsp, 8    ; move stack pointer back
+
+end:
+    ret        ; end
