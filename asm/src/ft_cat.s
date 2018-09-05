410a3a2cd033d6d76c3fc9192446e1d3aabf3873
Ryan SHEASBY <rsheasby@e4r5p1.wethinkcode.co.za>
2018-09-05 15:34:00 +0200

    WIP: Fix ft_cat for mach

--- asm/src/ft_cat.s
+++ asm/src/ft_cat.s
@@ -1,8 +1,8 @@
 section .text
     global _ft_cat
 
-extern malloc
-extern free
+extern _malloc
+extern _free
 
 _ft_cat:
 ; Validate input
@@ -10,28 +10,29 @@ _ft_cat:
     je end      ;
 
 ; Allocate memory
-    push r12 ; save callee-saved registers
-    push r13 ;
+    push r12   ; save callee-saved registers
+    push r13   ;
+    sub rsp, 8 ; pad stack to 16 bytes
 
     mov r13, rdi ; preserve file descriptor
     mov rdi, 256 ; how many bytes to allocate
-    call malloc  ; allocate memory
+    call _malloc ; allocate memory
     mov r12, rax ; put buffer pointer in r12
 
 l1:
 ; Read from file to buffer
-    mov rdi, r13 ; get fd
-    mov rsi, r12 ; get buffer
-    mov rdx, 256 ; read up to 256 bytes
-    mov rax, 0   ; use "read" syscall
-    syscall      ; read string
+    mov rdi, r13       ; get fd
+    mov rsi, r12       ; get buffer
+    mov rdx, 256       ; read up to 256 bytes
+    mov rax, 0x2000003 ; use "read" syscall
+    syscall            ; read string
 
 ; Write from buffer to stdout
-    mov rdi, 1   ; write to stdout
-    mov rsi, r12 ; get buffer
-    mov rdx, rax ; get length read
-    mov rax, 1   ; use "write" syscall
-    syscall      ; write string
+    mov rdi, 1         ; write to stdout
+    mov rsi, r12       ; get buffer
+    mov rdx, rax       ; get length read
+    mov rax, 0x2000004 ; use "write" syscall
+    syscall            ; write string
 
 ; Loop
     cmp rax, 256 ; check if 256 bytes written
@@ -39,7 +40,9 @@ l1:
 
 ; Cleanup
     mov rdi, r12 ; get buffer pointer
-    call free    ; free buffer
+    call _free    ; free buffer
+
+    add rsp, 8   ; remove padding from stack
     pop r13      ; restore callee-saved registers
     pop r12      ;
 
