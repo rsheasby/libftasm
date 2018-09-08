db84489bbe652fa0567df877d87f504aa19ae59e
Ryan SHEASBY <rsheasby@e4r17p6.wethinkcode.co.za>
2018-09-08 11:40:18 +0200

    Fix ft_isdigit formatting

--- asm/src/ft_isdigit.s
+++ asm/src/ft_isdigit.s
@@ -4,7 +4,7 @@ section .text
 _ft_isdigit:
     cmp rdi, '0' ; check if between '0' and '9'. If not, result is false.
     jl false     ;
-    cmp rdi,'9'  ;
+    cmp rdi, '9' ;
     jg false     ;
 
 true:
